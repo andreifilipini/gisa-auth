ccb86d4fdb94b34576e6b7582cb4f63ddf3603c3
Andrei Filipini <andrei.filipini@sigma.com.br>
2022-02-07 21:34:51 -0300

    melhora jwtTokenUtil

--- src/main/java/com/gisa/gisaauth/GisaAuthApplication.java
+++ src/main/java/com/gisa/gisaauth/GisaAuthApplication.java
@@ -3,7 +3,7 @@ package com.gisa.gisaauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.gisa")
 public class GisaAuthApplication {
 
 	public static void main(String[] args) {
